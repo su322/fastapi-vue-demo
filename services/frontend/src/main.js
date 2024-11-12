6cb5f7c22e860814469865aa8dd77b70dfc7977e
su322 <85404658+su322@users.noreply.github.com>
2024-11-12 20:29:08 +0800

    vue setup

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from "vue";
+import axios from 'axios';
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;  // 允许跨域请求时携带凭证（如 cookies）
+axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
+
+app.use(router);
+app.mount("#app");  // 将 Vue 应用实例挂载到 HTML 中 id 为 app 的元素上
