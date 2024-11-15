aafd90a5b8f93acca53631ab3f5c723af1889df7
su322 <85404658+su322@users.noreply.github.com>
2024-11-15 19:38:26 +0800

    添加注释

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -58,16 +58,23 @@ const router = createRouter({
   routes
 })
 
+// 在路由跳转前执行的钩子函数
 router.beforeEach((to, _, next) => {
+  // 检查即将进入的路由是否需要认证
   if (to.matched.some(record => record.meta.requiresAuth)) {
+    // 如果用户已经登录
     if (store.getters.isAuthenticated) {
+      // 允许导航
       next();
       return;
     }
+    // 如果用户未登录，重定向到登录页面
     next('/login');
   } else {
+    // 如果不需要认证，允许导航
     next();
   }
 });
+
 
 export default router
