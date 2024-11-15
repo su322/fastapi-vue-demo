df32fc62340123d320b2d7455cdd65e5cc517fde
su322 <85404658+su322@users.noreply.github.com>
2024-11-15 16:02:02 +0800

    vuex

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 const app = createApp(App);
 
-axios.defaults.withCredentials = true;  // 允许跨域请求时携带凭证（如 cookies）
+axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
 
 app.use(router);
-app.mount("#app");  // 将 Vue 应用实例挂载到 HTML 中 id 为 app 的元素上
+app.use(store);
+app.mount("#app");
